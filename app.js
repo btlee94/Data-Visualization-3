557580386d34f107cbdd4a2673956863fb194233
btlee94 <brandonl94@live.ca>
2017-11-28 12:30:33 -0700

    added age options

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-﻿/*
-var colour = d3.scaleOrdinal()
-    .range(["#FFD180", "#FFD180", "#FFCCBC", "#FFCCBC", "#FFCCBC", "#FF9E80", "#FF9E80", "#FF9E80",
-        "#FF8A80", "#FF8A80", "#FF8A80", "#FF80AB", "#FF80AB", "#FF80AB", "#EA80FC", "#EA80FC",
-        "#B388FF", "#B388FF", "#B388FF", "#8C9EFF", "#8C9EFF", "#8C9EFF", "#82B1FF", "#82B1FF",
-        "#82B1FF", "#80D8FF", "#80D8FF", "#80D8FF", "#00E5FF", "#00E5FF", "#00E5FF", "#1DE9B6",
-        "#1DE9B6", "#B9F6CA", "#B9F6CA", "#B9F6CA", "#FFFF8D", "#FFFF8D", "#FFE57F", "#FFE57F",
-        "#FFE57F"]);
-        */
-
+﻿
 var colour = ["#FF8A80", "#82B1FF", "#69F0AE", "#FFD180", "#EA80FC"];
 
 var width = screen.width * 0.70;
@@ -79,6 +70,44 @@ d3.csv("obesityData.csv", function (error, data) {
             return d["LocationAbbr"];
         })
 
+
+    var legend = svg.selectAll(".legend")
+        .data(colour)
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d, i) {
+            var LegendHeight = 13 + 20;
+            var horizontal = (width * 0.9);
+            var vertical = i * LegendHeight + (height * 0.35);
+            return "translate(" + horizontal + "," + vertical + ")";
+        });
+
+    legend.append("circle")
+        .attr("r", 13)
+        .style("fill", function (d, i) {
+            return colour[i];
+        });
+
+    legend.append("text")
+        .attr("x", 20)
+        .attr("y", 5)
+        .style("fill", "#FAFAFA")
+        .style("font-family", "'Raleway', sans-serif")
+        .style("font-size", "15")
+        .text(function (d, i) {
+            if (i == 0)
+                return "West";
+            if (i == 1)
+                return "Midwest";
+            if (i == 2)
+                return "Northeast";
+            if (i == 3)
+                return "South";
+            else
+                return "National";
+        });
+
     sim
         .nodes(gData)
         .force("collisions", d3.forceCollide(function (d) {
@@ -86,14 +115,56 @@ d3.csv("obesityData.csv", function (error, data) {
         }))
         .on('tick', updatePosition)
 
-    radio1.addEventListener("click", radioUpdateString);
-    radio2.addEventListener("click", radioUpdateString);
-    radio3.style.display = "none";
-    radio4.style.display = "none";
     slider.oninput = function () {
         year.innerHTML = this.value;
         sliderUpdateString();
     }
+
+    radio1.addEventListener("click", radioUpdateString);
+    radio2.addEventListener("click", radioUpdateString);
+    radio3.addEventListener("click", radioUpdateString);
+    radio4.addEventListener("click", radioUpdateString);
+    radio5.addEventListener("click", radioUpdateString);
+    radio6.addEventListener("click", radioUpdateString);
+    radio7.addEventListener("click", radioUpdateString);
+    radio8.addEventListener("click", radioUpdateString);
+
+    radioGen.addEventListener("click", setRadios);
+    radioAge.addEventListener("click", setRadios);
+
+    radio3.style.display = "none";
+    radio4.style.display = "none";
+    radio5.style.display = "none";
+    radio6.style.display = "none";
+    radio7.style.display = "none";
+    radio8.style.display = "none";
+    
+
+
+    function setRadios() {
+        if (this.id == "radioGen") {
+            radio1.style.display = "block";
+            radio2.style.display = "block";
+            radio3.style.display = "none";
+            radio4.style.display = "none";
+            radio5.style.display = "none";
+            radio6.style.display = "none";
+            radio7.style.display = "none";
+            radio8.style.display = "none";
+        }
+        else if (this.id == "radioAge") {
+            radio1.style.display = "none";
+            radio2.style.display = "none";
+            radio3.style.display = "block";
+            radio4.style.display = "block";
+            radio5.style.display = "block";
+            radio6.style.display = "block";
+            radio7.style.display = "block";
+            radio8.style.display = "block";
+        }
+        
+    }
+
     
 
     function updatePosition() {
@@ -126,10 +197,24 @@ d3.csv("obesityData.csv", function (error, data) {
     function radioUpdateString() {
         var currYear = year.innerHTML.slice(-2); //extract last 2 digits of year
 
-        if (this.innerText == "Male")
+        if (this.id == "radio1")
             dataString = "MALE" + currYear;
-        else if (this.innerText == "Female")
+        else if (this.id == "radio2")
             dataString = "FEMALE" + currYear;
+        else if (this.id == "radio3")
+            dataString = "AGEYR1824-" + currYear;
+        else if (this.id == "radio4")
+            dataString = "AGEYR2534-" + currYear;
+        else if (this.id == "radio5")
+            dataString = "AGEYR3544-" + currYear;
+        else if (this.id == "radio6")
+            dataString = "AGEYR4554-" + currYear;
+        else if (this.id == "radio7")
+            dataString = "AGEYR5564-" + currYear;
+        else if (this.id == "radio8")
+            dataString = "AGEYR65PLUS-" + currYear;
+
+        console.log(dataString);
 
         updateBubbles();
     }
@@ -151,42 +236,6 @@ d3.csv("obesityData.csv", function (error, data) {
     }
 
 
-    var legend = svg.selectAll(".legend")
-            .data(colour)
-            .enter()
-            .append("g")
-            .attr("class", "legend")
-            .attr("transform", function (d, i) {
-                var LegendHeight = 13 + 20;
-                var horizontal = (width * 0.9);
-                var vertical = i * LegendHeight + (height * 0.35);
-                return "translate(" + horizontal + "," + vertical + ")";
-            });
-
-        legend.append("circle")
-            .attr("r", 13)
-       .style("fill", function (d, i) {
-           return colour[i];
-       });
-
-        legend.append("text")
-            .attr("x", 20)
-            .attr("y", 5)
-            .style("fill", "#FAFAFA")
-            .style("font-family", "'Raleway', sans-serif")
-            .style("font-size", "15")
-            .text(function (d, i) {
-                if (i == 0)
-                    return "West";
-                if (i == 1)
-                    return "Midwest";
-                if (i == 2)
-                    return "Northeast";
-                if (i == 3)
-                    return "South";
-                else
-                    return "National";
-            });
     
 
 })
